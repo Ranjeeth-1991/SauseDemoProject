43032ea3b596789bff3511876fdb62f6a510f75e
Ranjeethkumar Kura <ranjeeth.since1991@gmail.com>
2025-07-26 20:34:18 +0530

    Constructors

---
+++ ConstructorsDemo.cs
@@ -0,0 +1,20 @@
+﻿namespace ConstructorsDemo
+{
+    class ConstructorsDemo
+    {
+        static int y;
+        int x;
+        static ConstructorsDemo()
+        {
+            Console.WriteLine("Static Constructor is Called.");
+        }
+        public ConstructorsDemo()
+        {
+            Console.WriteLine("Non-Static Constructor is Called.");
+        }
+        static void Main()
+            {
+
+            }
+    }
+}
